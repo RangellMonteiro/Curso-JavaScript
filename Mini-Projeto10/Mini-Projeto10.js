9a1b0bab5ba8d290db3238cce6a26082790df7b6
RangellMonteiro <rangell_monteiro@hotmail.com>
2024-11-22 04:19:03 -0300

    Numeros primos

---
+++ Mini-Projeto10/Mini-Projeto10.js
@@ -0,0 +1,17 @@
+exibirnumerosPrimos(15);
+
+function exibirnumerosPrimos(limite){
+    for(let numero = 2; numero <= limite; numero++){
+        if(NumeroPrimo(numero)) console.log(numero);
+    }
+}
+
+function NumeroPrimo(numero){
+    for (let divisor = 2; divisor < numero ; divisor ++){
+        if(numero % divisor === 0){
+            return false;
+
+        }
+    }
+    return true;
+}
